a2c4f3fec648b0ffdf0c26dfe30ff0ec3e3b1edf
Fúlvio <deadfubs@gmail.com>
2023-08-01 14:14:55 -0300

    authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-  return (
-    <p>Hello! Admin Dashboard</p>
+const SetupPage = () => {
+  return ( 
+    <div className='p-4'>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
+
+export default SetupPage;
